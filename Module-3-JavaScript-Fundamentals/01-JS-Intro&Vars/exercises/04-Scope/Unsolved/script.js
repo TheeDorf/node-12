372b7fd2025d6fa5a60c46e373146df1d4a8c17d
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-09 12:18:15 -0800

    section 4

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/04-Scope/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/04-Scope/Unsolved/script.js
@@ -1,5 +1,5 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
-var shout = "Shout";
+const shout = "Shout";
 
 function justShout() {
   console.log(shout + ", " + shout); 
@@ -17,20 +17,22 @@ shoutItAllOut();
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
 
 function sayLions() {
-  var animal = "Lions"; 
+  let animal = "Lions"; 
   console.log(animal);
   return;
 }
+let animal = "Tigers"
+
 function sayTigers() {
-  var animal = "Tigers"
   console.log("and " + animal + " and "); 
   return;
 }
-// TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!". 
-var bears = "Bears";
 
-function sayBears() {
-   // remove this var bears = "Pandas";
+// TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!". 
+const bears = "Bears";
+console.log(bears);
+
+function sayBears() {  
     console.log(bears + "! OH MY!");
     return;
 }
@@ -40,12 +42,11 @@ sayTigers();
 sayBears();
 
 // TODO: The variable 'sing' should be declared once in the local scope.
-// var sing = "Sing"; 
 
 function singAlong() {
-  var sing = "Sing"
+  const sing = "Sing"
   console.log(sing + ",");
-  var singASong = function () {
+  const singASong = function () {
     console.log(sing + " a Song."); 
   };
   singASong();
