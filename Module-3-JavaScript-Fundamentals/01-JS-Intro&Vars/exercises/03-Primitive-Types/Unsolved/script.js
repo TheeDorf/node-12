f27049ccb27d744e06d390e968d1c49c2759de0e
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-09 09:37:28 -0800

    exercise 3

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/03-Primitive-Types/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/03-Primitive-Types/Unsolved/script.js
@@ -6,18 +6,20 @@ var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof one); 
-console.log(typeof two); 
-console.log(typeof three); 
-console.log(typeof four); 
-console.log(typeof five);
-console.log(typeof six);
+//first one is a number, second is a string, 3rd is a boolean, 4th is a number,5 is a string, 6 is undefined
+console.log(typeof one); // number //
+console.log(typeof two); // string //
+console.log(typeof three); //boolean //
+console.log(typeof four); //number //
+console.log(typeof five); // string//
+console.log(typeof six); //undefined //
 
 four = "Hello!";
 five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof four);
-console.log(typeof five);
-console.log(typeof six);
+// 
+console.log(typeof four); // string //
+console.log(typeof five); // boolean //
+console.log(typeof six); // number //
