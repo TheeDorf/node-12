7673c988eeea03340843be4df2bd120731b50df0
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-09 08:42:10 -0800

    complete exercise 2

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,21 @@
 // Write Your JavaScript Code Here
+var personName;
+
+var personName = "AJ";
+var personPets = 1;
+var funFact = "I have a boston terrier";
+
+
+personName = "Cole";
+personPet = "1";
+funFact = "I climbed mt. kilimajaro";
+
+console.log(personName);
+console.log(personPets);
+console.log(funFact);
+
+console.log("My name is "  +  personName);
+console.log(personName + " has " + personPet + " pet");
+console.log("my funfact is " + funFact);
+
+
