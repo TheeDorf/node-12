d9edc34d0fe04d1527527e89cad598844566f742
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-10 12:14:51 -0800

    complate exercise

--- zeAlgos/01-arithmetic/Unsolved/arithmetic.js
+++ zeAlgos/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,23 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
 
-var subtract = function(num1, num2) {};
+var add = function(num1, num2) {
+    console.log(num1+num2);
+    var sum=num1+num2
+    return num1+num2;
+};
 
-var multiply = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    console.log(num1-num2);
+    return num1-num2
+};
 
-var divide = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    console.log(num1*num2);
+    return num1*num2
+};
+
+var divide = function(num1, num2) {
+    console.log(num1/num2)
+    return num1/num2
+};
